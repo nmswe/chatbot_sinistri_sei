7aafa086c21d756c8600bd083003afdf304a2b0f
Irene Fabbri <irenefabbri.ed@gmail.com>
2025-09-09 16:15:27 +0200

    Edit: added GoogleAPI  call in app/api/chat

--- sinistri-sei/src/app/api/chat/route.ts
+++ sinistri-sei/src/app/api/chat/route.ts
@@ -1,14 +1,16 @@
-import { openai } from "@ai-sdk/openai";
+import { google } from "@ai-sdk/google";
 import { convertToModelMessages, streamText, UIMessage } from "ai";
+import { VillainArray } from "../../../../lib/Villain";
+
 
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
     const { messages }: { messages: UIMessage[] } = await req.json();
-
+    
     const result = streamText({
-        model: openai("gpt-4.1"),
-        system: "You are a helpful assistant.",
+        model: google('gemini-2.5-flash'),
+        system: VillainArray[0].toString(),
         messages: convertToModelMessages(messages),
     });
 
