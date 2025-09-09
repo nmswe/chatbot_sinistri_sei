14df8d4fb2629d634e888e9ee05dcca9932fe383
Irene Fabbri <irenefabbri.ed@gmail.com>
2025-09-09 18:00:49 +0200

    Added: VillainService starting implementation

--- sinistri-sei/src/app/api/chat/route.ts
+++ sinistri-sei/src/app/api/chat/route.ts
@@ -1,16 +1,16 @@
 import { google } from "@ai-sdk/google";
 import { convertToModelMessages, streamText, UIMessage } from "ai";
 import { VillainArray } from "../../../../lib/Villain";
+import { getNextVillain } from "../../../../lib/VillainService";
 
 
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-    const { messages }: { messages: UIMessage[] } = await req.json();
-    
+    const { messages }: { messages: UIMessage[]} = await req.json();
     const result = streamText({
         model: google('gemini-2.5-flash'),
-        system: VillainArray[0].toString(),
+        system: getNextVillain(0).toPromptString(),
         messages: convertToModelMessages(messages),
     });
 
