c53e1a2f76e4e0125a1f21d624534edc70d62821
Nicola Marmugi <marmu1@hotmail.it>
2025-09-11 21:39:54 +0200

    fix

--- sinistri-sei/src/app/hook/useChat.ts
+++ sinistri-sei/src/app/hook/useChat.ts
@@ -1,10 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ChatStatus, Message, SendMessageParams, VillainState } from "../types/useChatTypes/useChat";
 
 function useChat() {
     const [messages, setMessages] = useState<Message[]>([]);
     const [status, setStatus] = useState<ChatStatus>("ready");
     const [villainState, setVillainState] = useState<VillainState>({ currentIndex: 0, defeatCounter: 0 });
+    
+    useEffect(() => {
+        const storedMessages = localStorage.getItem("messages");
+        const storedVillainState = localStorage.getItem("villainState");
+
+        if (storedMessages) {
+            setMessages(JSON.parse(storedMessages));
+        } else {
+            localStorage.setItem("messages", JSON.stringify([]));
+        }
+
+        if (storedVillainState) {
+            setVillainState(JSON.parse(storedVillainState));
+        } else {
+            localStorage.setItem("villainState", JSON.stringify({ currentIndex: 0, defeatCounter: 0 }));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("messages", JSON.stringify(messages));
+    }, [messages]);
+
+    useEffect(() => {
+        localStorage.setItem("villainState", JSON.stringify(villainState));
+    }, [villainState]);
+
+    useEffect(() => {
+        if (villainState.currentIndex === 0 && villainState.defeatCounter === 6) {
+            localStorage.removeItem("messages");
+            localStorage.removeItem("villainState");
+            setMessages([]);
+            setVillainState({ currentIndex: 0, defeatCounter: 0 });
+        }
+    }, [villainState]);
 
     const sendMessage = async ({ text }: SendMessageParams) => {
         const userMessage: Message = {
@@ -38,7 +72,6 @@ function useChat() {
             setStatus("ready");
         }
     };
-
 
     return { messages, status, villainState, sendMessage };
 }
